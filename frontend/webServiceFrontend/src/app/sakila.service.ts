a1838993d4078d3010e40c2acfc01fdb6d6c789e
Kin <ckwojai@gmail.com>
2019-05-31 22:27:05 -0700

    Setting up Flask+Gunicorn+Nginx successful in Google VM, YAY

--- frontend/webServiceFrontend/src/app/sakila.service.ts
+++ frontend/webServiceFrontend/src/app/sakila.service.ts
@@ -6,7 +6,8 @@ import { Observable, of } from 'rxjs';
   providedIn: 'root'
 })
 export class SakilaService {
-  private baseUrl: string = 'http://localhost:5000';
+  private baseUrl: string = 'http://35.222.91.21';
+  // private baseUrl: string = 'http://localhost:5000';
   getFilms(): Observable<any[]> {
     return this.http.get<any[]>(this.baseUrl);
   }
